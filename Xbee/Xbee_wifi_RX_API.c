a40a5bd063e97dfafa119e99796d22b2908f5139
Michael Bartling <michael.bartling15@gmail.com>
2013-11-04 03:26:44 -0600

    Update Xbee_wifi_RX_API.c
    Added Authorship information and copyright

--- Xbee/Xbee_wifi_RX_API.c
+++ Xbee/Xbee_wifi_RX_API.c
@@ -1,3 +1,12 @@
+/**
+ * \author Michael Bartling bart4128@tamu.edu
+ * \author Rabih Abifaraj   
+ * \date 11/4/2013
+ * \version 1.0
+ * 
+ * \copyright The MIT License (MIT)
+ */
+
 #include <Xbee_wifi_RX_API.h>
 #define INPUT  /*Input*/
 #define OUTPUT /*Output*/
