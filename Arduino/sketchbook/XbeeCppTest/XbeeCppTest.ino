59eeda0ddfce4cf885c7a43c6895730d9a6209e1
Michael Bartling <michael.bartling15@gmail.com>
2014-02-01 12:20:37 -0600

    Initial commit of the xbee cpp test

--- Arduino/sketchbook/XbeeCppTest/XbeeCppTest.ino
+++ Arduino/sketchbook/XbeeCppTest/XbeeCppTest.ino
@@ -1,13 +1,30 @@
-#include <XBeeS6.h>
+#include <XbeeS6.h>
+
+//#include <XBeeS6.h>
 
 //#include "XbeeS6.h"
+#define BAUD_RATE 115200
+
+Tx64Packet tx_packet;
 
 void setup()
 {
+  Serial.begin(BAUD_RATE);
+  Serial.println("Starting the Receiver!");
   
+  tx_packet.set_Address(0x00000000C0A80064);
+  tx_packet.push_back(0x15);
+  tx_packet.push_back(0x16);
 }
 
 void loop()
 {
+  uint16_t length = tx_packet.packet_buf();
   
+  for(int i = 0; i < length; i++)
+  {
+    Serial.print(tx_buffer[i], HEX); 
+    Serial.print(" ");
+  }
+  Serial.print('\n');
 }
