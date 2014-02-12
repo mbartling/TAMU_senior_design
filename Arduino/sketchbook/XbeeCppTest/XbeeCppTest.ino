667ca064b819922d970bcd4cad18b9baa9b3caf8
Michael Bartling <michael.bartling15@gmail.com>
2014-02-11 23:16:17 -0600

    Added some time capabilities

--- Arduino/sketchbook/XbeeCppTest/XbeeCppTest.ino
+++ Arduino/sketchbook/XbeeCppTest/XbeeCppTest.ino
@@ -28,16 +28,19 @@ const int led_pin = 11;  // 11=Teensy 2.0, 6=Teensy 1.0, 16=Benito
 const int led_on = HIGH;
 const int led_off = LOW;
 
-time_t start_time;
+volatile time_t start_time;
 time_t current_time;
 void setup()
 {
+  start_time = now();
   pinMode(led_pin, OUTPUT);
 	digitalWrite(led_pin, led_off);
 	digitalWrite(reset_pin, HIGH);
 	pinMode(reset_pin, OUTPUT);
   Serial.begin(BAUD_RATE);
   Serial.println("Starting the Receiver!");
+  Serial.print("Starting time: ");
+  Serial.println(start_time);
   
   tx_packet.set_Address(0x00000000C0A80165);  
   //tx_packet.set_Address(0x00000000FFFFFFFF);
@@ -94,6 +97,7 @@ if(enable == 1)
     delay(2000);
   uint16_t length = tx_packet.prepare2send();
   uint16_t i;
+  
   for(i = 0; i < length; i++)
   {
     //Uart.print(tx_buffer1[i], BYTE);
@@ -140,6 +144,9 @@ if(enable == 3) //For command mode use write for repsponse use print
  if (Uart.available()) {
 		c = Uart.read();
 //		Serial.write(c);
+                 current_time = now();
+                Serial.print("current time: ");
+                Serial.println(current_time);
                 if(c == 0x7E) Serial.println();
                 Serial.print(c, HEX);
                 Serial.print(" ");
