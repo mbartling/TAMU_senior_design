f766dd236d1d7282601bfbc3b229c9cb96a67249
Michael Bartling <michael.bartling15@gmail.com>
2014-02-22 11:53:03 -0600

    quick serial update

--- Python/get_serial.py
+++ Python/get_serial.py
@@ -1,11 +1,39 @@
 #! /usr/bin/env python
 
 import serial
+import sys
+from zig_packet import myparse
 
-xbee = serial.Serial('/dev/ttyACM0', 115200)
+#xbee = serial.Serial('/dev/ttyACM0', 115200)
+xbee  = serial.Serial()
+xbee.baudrate = 115200
+xbee.port = '/dev/ttyACM0'
 
-if serial.isOpen():
-    for line in xbee:
-        print line
+if xbee.isOpen():
+    xbee.close()
+xbee.open()
+print xbee
+xbee.write("?")
+if xbee.isOpen():
+    for line in xbee:    
+        line = line.strip()
+        packet = line.split()
+        sf = packet[0]
+        lenH = packet[1]
+        lenL = packet[2]
+        apiFid = packet[3]
+        address = packet[4:12]
+        rssi = packet[12]
+        payload = packet[13:]
+        if sf == '7E':
+            print address , "rssi = ", rssi
+        
+        #print line
+        #rssi = myparse(line)
+        #print address , rssi
+        #print line
+        #if(sys.stdin.read() == 'x'):
+        #    break
 
-xbee.close
+print "Closing Xbee"
+xbee.close()
