00823fb80bdca0d64f1a7b5a77fcd6db71597604
Michael Bartling <michael.bartling15@gmail.com>
2014-03-25 14:29:37 -0500

    Added weighted averaging between pass 1 and pass 2 of the algorithm

--- Python/Mapping/inv_map.py
+++ Python/Mapping/inv_map.py
@@ -6,14 +6,14 @@ from numpy.linalg import inv
 
 DEBUGMODE = 1
 if DEBUGMODE == 1:
-	from mpl_toolkits.mplot3d import Axes3D
-	import matplotlib.pyplot as plt
-	import matplotlib.cm as cm
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib.pyplot as plt
+    import matplotlib.cm as cm
 
 # Parameters
 xSize = 300
 ySize = 500
-
+weight = 3
 # Lists to maintain
 xCoords = []
 yCoords = []
@@ -21,32 +21,32 @@ rssi = []
 fptr = open('testpoints.out', 'r')
 
 def pass1(i, j, xmean, ymean, coefmat):
-	xs = i-xmean
-	ys = j-ymean
-	temp = numpy.dot(numpy.array([xs**2, ys**2, xs*ys, xs, ys, 1]), coefmat)
-	#print temp
-	return temp
+    xs = i-xmean
+    ys = j-ymean
+    temp = numpy.dot(numpy.array([xs**2, ys**2, xs*ys, xs, ys, 1]), coefmat)
+    #print temp
+    return temp
 
 def pass2(i, j, xmean, ymean, coefmat):
-	xs = i-xmean
-	ys = j-ymean
-	func = numpy.array([xs**2, ys**2, 2*xs*ys])
-	#print func.T
-	#print coefmat
-	temp = numpy.dot( func.T , coefmat)
-	#print temp
-	temp = numpy.exp(-temp)
-	#print temp
-	return temp
+    xs = i-xmean
+    ys = j-ymean
+    func = numpy.array([xs**2, ys**2, 2*xs*ys])
+    #print func.T
+    #print coefmat
+    temp = numpy.dot( func.T , coefmat)
+    #print temp
+    temp = numpy.exp(-temp)
+    #print temp
+    return temp
 
 #for line in sys.stdin:
 for line in fptr:
-	line = line.strip()
-	(xCoordIn, yCoordIn, rssiIn) = line.split(',')
-	# print xCoordIn, yCoordIn, rssiIn
-	xCoords.append(xCoordIn)
-	yCoords.append(yCoordIn)
-	rssi.append(rssiIn)
+    line = line.strip()
+    (xCoordIn, yCoordIn, rssiIn) = line.split(',')
+    # print xCoordIn, yCoordIn, rssiIn
+    xCoords.append(xCoordIn)
+    yCoords.append(yCoordIn)
+    rssi.append(rssiIn)
 
 fptr.close()
 xloc = numpy.asarray(xCoords, dtype='int32')
@@ -83,19 +83,26 @@ print coefmat
 gamemap = numpy.zeros((xSize, ySize))
 
 for i in range(xSize):
-	for j in range(ySize):
-		gamemap[i,j] = pass1(i, j, xmean, ymean, coefmat)
+    for j in range(ySize):
+        gamemap[i,j] = pass1(i, j, xmean, ymean, coefmat)
 
 gamemap = -gamemap/numpy.sum(numpy.sum(numpy.abs(gamemap)))
 
 # Display Gamemap
 if DEBUGMODE == 1:
-	plt.figure()
-	CS = plt.contourf(gamemap)
-	#plt.show()
+    plt.figure()
+    CS = plt.contourf(gamemap, origin='image')
+    plt.colorbar()
+
+    plt.figure()
+    im = plt.imshow(gamemap, interpolation='bicubic',origin='image', cmap=cm.jet)
+    plt.colorbar()
+    #plt.show()
 
 (xe,ye) = numpy.nonzero(gamemap == numpy.max(numpy.max(gamemap)))
 
+#Save the original game map, we might be able to exploit it
+gamemapFat = gamemap
 print xe, ye
 
 ## Begin Pass 2
@@ -122,17 +129,28 @@ print coefmat
 gamemap = numpy.zeros((xSize, ySize))
 
 for i in range(xSize):
-	for j in range(ySize):
-		gamemap[i,j] = pass2(i, j, xe, ye, coefmat)
+    for j in range(ySize):
+        gamemap[i,j] = pass2(i, j, xe, ye, coefmat)
 
 
 # Display Gamemap
 if DEBUGMODE == 1:
-	plt.figure()
-	CS = plt.contourf(gamemap)
-	#X,Y = numpy.mgrid[range(xSize), range(ySize)]
-	#ax = fig.gca(projection='3d')
-	#CS = ax.plot_wireframe(gamemap, rstride=1, cstride=1)
-	plt.figure()
-	im = plt.imshow(gamemap, interpolation='spline36',origin='upper', cmap=cm.jet)
-	plt.show()
+    plt.figure()
+    CS = plt.contourf(gamemap, origin='image')
+    plt.colorbar()
+    #X,Y = numpy.mgrid[range(xSize), range(ySize)]
+    #ax = fig.gca(projection='3d')
+    #CS = ax.plot_wireframe(gamemap, rstride=1, cstride=1)
+    plt.figure()
+    im = plt.imshow(gamemap, interpolation='spline36',origin='image', cmap=cm.jet)
+    plt.colorbar()
+        
+    plt.figure()
+    gamemapFat += numpy.max(numpy.max(abs(gamemapFat)))
+    gamemapFat *= 1.0/numpy.max(numpy.max(numpy.abs(gamemapFat)))
+    im = plt.imshow(gamemapFat, interpolation='spline36',origin='image', cmap=cm.jet)
+    plt.colorbar()
+
+    plt.figure()
+    im = plt.imshow((weight*gamemap+gamemapFat)/(weight+1),interpolation='bicubic',origin='image', cmap=cm.jet)
+    plt.show()
